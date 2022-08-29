234f007a29e5fa93deb7d9d52ec7d330f6d2660d
sweetn3ss <40582849+sweetn3ss@users.noreply.github.com>
2022-08-29 16:59:12 -0700

    removed icons and emptied icon fields

--- CamoSwapDemo_github/config.cpp
+++ CamoSwapDemo_github/config.cpp
@@ -13,7 +13,7 @@ class cfgPatches {
 	};
 };
 
-class Extended_PostInit_EventHandlers {  // put these files in the same folder as this config
+class Extended_PostInit_EventHandlers {
 	class camoCoefKey {
 		init = "spawn CamoSwapDemo_github_fnc_camoCoef_Key";
 	};
@@ -49,227 +49,227 @@ class cfgVehicles {
                 displayName="Personalization";
                 condition="(uniform player) in [""82nd_BDU_Morph_BASE"", ""82nd_BDU_Morph_ROLLED"", ""82nd_BDU_Morph_Short""]";
                 statement="";
-                icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_blank.paa";
+                icon = "";
                 class Camo_Select {
                     displayName="Change Camouflage";
                     condition="(uniform player) in [""82nd_BDU_Morph_BASE"", ""82nd_BDU_Morph_ROLLED"", ""82nd_BDU_Morph_Short""]";
                     statement="";
-                    icon = "\82nd_Armor_New\data\ui\82nd_ui_camo_menu.paa";
+                    icon = "";
                     class Urban {
                         displayName="Urban";
                         condition="true";
                         statement="[player, ""Urban""] call personalizeArmor_fnc_camoChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_camo_BLK.paa";
+                        icon = "";
                     };
                     class Desert {
                         displayName="Desert";
                         condition="true";
                         statement="[_player, ""Desert""] call personalizeArmor_fnc_camoChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_camo_DES.paa";
+                        icon = "";
                     };
                     class Winter {
                         displayName="Winter";
                         condition="true";
                         statement="[_player, ""Winter""] call personalizeArmor_fnc_camoChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_camo_SNO.paa";
+                        icon = "";
                     };
                     class Woodland {
                         displayName="Woodland";
                         condition="true";
                         statement="[_player, ""Woodland""] call personalizeArmor_fnc_camoChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_camo_WDL.paa";
+                        icon = "";
                     };
                 };
 				class Vest_Select_Air {
                     displayName="Change Vests (Air)";
                     condition="(uniform player) in [""82nd_BDU_Morph_BASE"", ""82nd_BDU_Morph_ROLLED"", ""82nd_BDU_Morph_Short""]";
                     statement="";
-                    icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_airassault.paa";
+                    icon = "\CamoSwapDemo_github\data\ui\82nd_ui_m52a_airassault.paa";
                     class AirRifleman {
                         displayName="Air Assault";
                         condition="true";
                         statement="[player, ""AirAssault""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_airassault.paa";
+                        icon = "";
                     };
                     class AirCorpsman {
                         displayName="Corpsman";
                         condition="true";
                         statement="[player, ""AirMedic""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_airmed.paa";
+                        icon = "";
                     };
                     class AirMG {
                         displayName="Autorifleman";
                         condition="true";
                         statement="[player, ""AirMG""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_airauto.paa";
+                        icon = "";
                     };
                     class AirGrenadier {
                         displayName="Grenadier";
                         condition="true";
                         statement="[player, ""AirGrenadier""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_airgren.paa";
+                        icon = "";
                     };
                     class AirMarksman {
                         displayName="Marksman";
                         condition="true";
                         statement="[player, ""AirMarksman""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_airmark.paa";
+                        icon = "";
                     };
                     class AirBreacher {
                         displayName="Breacher";
                         condition="true";
                         statement="[player, ""AirBreacher""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_airbrch.paa";
+                        icon = "";
                     };
                     class AirTeamLead {
 						displayName="Team Lead";
 						condition="true";
 						statement="[player, ""AirTeamLead""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_airlead.paa";
+                        icon = "";
 					};
                 };
 				class Vest_Select_Tyr {
                     displayName="Change Vests (Tyr)";
                     condition="(uniform player) in [""82nd_BDU_Morph_BASE"", ""82nd_BDU_Morph_ROLLED"", ""82nd_BDU_Morph_Short""]";
                     statement="";
-                    icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_blank.paa";
+                    icon = "";
                     class TyrRifleman {
                         displayName="Rifleman";
                         condition="true";
                         statement="[player, ""TyrRifleman""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_blank.paa";
+                        icon = "";
                     };
                     class TyrCorpsman 
                         displayName="Corpsman";
                         condition="true";
                         statement="[player, ""TyrCorpsman""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_corpsman.paa";
+                        icon = "";
                     };
                     class TyrMG {
                         displayName="Autorifleman";
                         condition="true";
                         statement="[player, ""TyrMG""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_autorifleman.paa";
+                        icon = "";
                     };
                     class TyrGrenadier {
                         displayName="Grenadier";
                         condition="true";
                         statement="[player, ""TyrGrenadier""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_grenadier.paa";
+                        icon = "";
                     };
                     class TyrMarksman {
                         displayName="Marksman";
                         condition="true";
                         statement="[player, ""TyrMarksman""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_marksman.paa";
+                        icon = "";
                     };
                     class TyrBreacher {
                         displayName="Breacher";
                         condition="true";
                         statement="[player, ""TyrBreacher""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_breacher.paa";
+                        icon = "";
                     };
                     class TyrTeamLead {
 						displayName="Team Lead";
 						condition="true";
 						statement="[player, ""TyrTeamLead""] call personalizeArmor_fnc_vestChange;";
-						icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_teamlead.paa";
+						icon = "";
 					};
                 };
 				class Vest_Select_Slp {
                     displayName="Change Vests (Sleipnir)";
                     condition="(uniform player) in [""82nd_BDU_Morph_BASE"", ""82nd_BDU_Morph_ROLLED"", ""82nd_BDU_Morph_Short""]";
                     statement="";
-                    icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_blank.paa";
+                    icon = "";
                     class SlpRifleman {
                         displayName="Rifleman";
                         condition="true";
                         statement="[player, ""SlpRifleman""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_blank.paa";
+                        icon = "";
                     };
                     class SlpCorpsman {
                         displayName="Corpsman";
                         condition="true";
                         statement="[player, ""SlpCorpsman""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_corpsman.paa";
+                        icon = "";
                     };
                     class SlpMG {
                         displayName="Autorifleman";
                         condition="true";
                         statement="[player, ""SlpMG""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_autorifleman.paa";
+                        icon = "";
                     };
                     class SlpGrenadier {
                         displayName="Grenadier";
                         condition="true";
                         statement="[player, ""SlpGrenadier""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_grenadier.paa";
+                        icon = "";
                     };
                     class SlpMarksman {
                         displayName="Marksman";
                         condition="true";
                         statement="[player, ""SlpMarksman""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_marksman.paa";
+                        icon = "";
                     };
                     class SlpBreacher {
                         displayName="Breacher";
                         condition="true";
                         statement="[player, ""SlpBreacher""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_breacher.paa";
+                        icon = "";
                     };
                     class SlpTeamLead {
 						displayName="Team Lead";
 						condition="true";
 						statement="[player, ""SlpTeamLead""] call personalizeArmor_fnc_vestChange;";
-						icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_teamlead.paa";
+						icon = "";
 					};
                 };
 				class Vest_Select_Aux {
 					displayName="Change Vests (Branches)";
                     condition="(uniform player) in [""82nd_BDU_Morph_BASE"", ""82nd_BDU_Morph_ROLLED"", ""82nd_BDU_Morph_Short""]";
                     statement="";
-                    icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_crew.paa";
+                    icon = "";
 					class Crew {
 						displayName="Crew";
 						condition="true";
 						statement="[player, ""Crew""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_crew.paa";
+                        icon = "";
 					};
 					class Aviation {
 						displayName="Aviation";
 						condition="true";
 						statement="[player, ""Aviation""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_aviation.paa";
+                        icon = "";
 					};
 					class PJ {
 						displayName="Aviaiton PJ";
 						condition="true";
 						statement="[player, ""PJ""] call personalizeArmor_fnc_vestChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_m52a_aviation2.paa";
+                        icon = "";
 					};
 				};
 				class Uniform_Select {
                     displayName="Change Uniform";
                     condition="(uniform player) in [""82nd_BDU_Morph_BASE"", ""82nd_BDU_Morph_ROLLED"", ""82nd_BDU_Morph_Short""]";
                     statement="";
-                    icon = "\82nd_Armor_New\data\ui\82nd_ui_bdu_base.paa";
+                    icon = icon = "";
                     class Base {
                         displayName="Default";
                         condition="true";
                         statement="[player, ""Base""] call personalizeArmor_fnc_uniformChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_bdu_base.paa";
+                        icon = "";
                     };
                     class Rolled {
                         displayName="Rolled Sleeves";
                         condition="true";
                         statement="[_player, ""Rolled""] call personalizeArmor_fnc_uniformChange;";
-                        icon = "\82nd_Armor_New\data\ui\82nd_ui_bdu_rolled.paa";
+                        icon = "";
 					};
 					class Short {
 						displayName="Short Sleeves";
 						condition="true";
 						statement="[_player, ""Short""] call personalizeArmor_fnc_uniformChange;";
-						icon = "\82nd_Armor_New\data\ui\82nd_ui_bdu_short.paa";
+						icon = "";
                     };
                 };
 			};
